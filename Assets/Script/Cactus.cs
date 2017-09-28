a435e9a2a7e00b43673338da490c8ebf5a5d4f28
박민근 <agebreak@naver.com>
2017-09-28 11:40:05 +0900

    게임 매니저 추가

--- Assets/Script/Cactus.cs
+++ Assets/Script/Cactus.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cactus : MonoBehaviour {
+public class Cactus : MonoBehaviour
+{
+    public Vector3 speed;
+    public float randMin;
+    public float randMax;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // 생성될때 Y값을 랜덤한값(최소, 최대)으로 설정한다. 
+        //Vector3 pos = transform.position;
+        //pos.y = Random.Range(randMin, randMax);
+        //transform.position = pos;
+
+        float y = Random.Range(randMin, randMax);
+        transform.Translate(new Vector3(0, y, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        transform.position += speed * Time.deltaTime;
+    }
+
+    // 화면에서 사라지면 삭제된다. 
+    public void OnBecameInvisible()
+    {
+        // 내가 붙어있는 오브젝트를 파괴한다.
+        Destroy(gameObject);
+    }
+
+    
+
 }
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject cactus; // 생성할 선인장 프리팹
+    public float spawnTime = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        // 선인장을 프래팹에서 정해진 시간마다 생성한다. 
+        InvokeRepeating("MakeCactus", 0.0f, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // 선인장을 프래팹에서 정해진 시간마다 생성한다. 
+    void MakeCactus()
+    {
+        Instantiate(cactus);
+    }
+}
